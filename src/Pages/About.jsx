b8348f37be4ad6ad7fa172df5b7e3973844d08dc
neethuganesh25 <neethuganesh2001@gmail.com>
2024-07-18 20:50:21 +0530

    about updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Ratings from './Ratings';
-import { height } from '@fortawesome/free-brands-svg-icons/fa42Group';
+import './About.css'; 
 
 function About() {
   const cardRatings = [4, 5, 3, 4, 5, 4];
   return (
-
     <>
-    <Header />
+      <Header />
       <div className="row">
         <div className="col-md-1"></div>
         <div className="col-md-4">
           <img className='border w-100 shadow rounded-4 mt-5'
-           src="https://images.unsplash.com/photo-1566312235254-d4b52d8fb053?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlc3xlbnwwfHwwfHx8MA%3D%3D"
+            src="https://images.unsplash.com/photo-1566312235254-d4b52d8fb053?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlc3xlbnwwfHwwfHx8MA%3D%3D"
             height={300} alt="" />
         </div>
         <div className="col-md-5">
@@ -26,34 +25,30 @@ function About() {
         <div className="col-md-2"></div>
       </div>
       <div className="row">
-
         <div className="col-md-2"></div>
         <div className="col-md-3">
-          <Card className='mt-5  ' style={{ width: '80%' }}>
+          <Card className='mt-5' style={{ width: '80%' }}>
             <Card.Img variant="top" src="https://images.unsplash.com/photo-1609742022736-46c49c147da9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHJlY2lwZXMlMjBpdGVtc3xlbnwwfHwwfHx8MA%3D%3D" />
             <Card.Body>
-              <Card.Title>Chinese Hot Pot.</Card.Title>
+              <Card.Title>Chinese Hot Pot</Card.Title>
               <Card.Text>
-
-              Hot pot or hotpot, also known as steamboat, 
-              is a dish whereby a heat source placed on the 
-              dining table keeps a pot of soup stock simmering.
-               
-                <Ratings   rating={cardRatings[2]} />
+                Hot pot or hotpot, also known as steamboat, 
+                is a dish whereby a heat source placed on the 
+                dining table keeps a pot of soup stock simmering.
+                <Ratings rating={cardRatings[2]} />
               </Card.Text>
-
             </Card.Body>
           </Card>
         </div>
         <div className="col-md-3">
-          <Card className='mt-5  ' style={{ width: '80%' }}>
+          <Card className='mt-5' style={{ width: '80%' }}>
             <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2018/08/30/10/22/plum-cake-3641833_640.jpg" />
             <Card.Body>
               <Card.Title>Spring Rolls</Card.Title>
               <Card.Text>
                 Spring rolls are rolled appetizers or dim sum commonly found in Chinese and Southeast Asian cuisines
+                <Ratings rating={cardRatings[3]} />
               </Card.Text>
-              <Ratings rating={cardRatings[3]} />
             </Card.Body>
           </Card>
         </div>
@@ -63,26 +58,32 @@ function About() {
             <Card.Body>
               <Card.Title>Chow Mein</Card.Title>
               <Card.Text>
-              Chow mein is a dish of Chinese stir-fried noodles with vegetables and sometimes meat or tofu.
+                Chow mein is a dish of Chinese stir-fried noodles with vegetables and sometimes meat or tofu.
+                <Ratings rating={cardRatings[4]} />
               </Card.Text>
-              <Ratings rating={cardRatings[4]} />
             </Card.Body>
           </Card>
         </div>
+        <div className="col-md-1"></div>
       </div>
-      <div className="col-md-1"></div>
       <div className="row">
         <div className="col-md-2"></div>
-        <div className="col-md-10 w-75 mt-5">
-        <iframe width="866" height="487" src="https://www.youtube.com/embed/8L7V1eTaTnw" title="CHICKEN TIKKA NO OVEN | CHICKEN TIKKA KEBAB ON TAWA | CHICKEN TIKKA RECIPE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="col-md-8">
+          <div className="youtube-video  mt-5">
+            <iframe
+              src="https://www.youtube.com/embed/8L7V1eTaTnw"
+              title="CHICKEN TIKKA NO OVEN | CHICKEN TIKKA KEBAB ON TAWA | CHICKEN TIKKA RECIPE"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen>
+            </iframe>
+          </div>
         </div>
         <div className="col-md-2"></div>
       </div>
-
-
       <Footer />
     </>
   )
 }
 
-export default About
+export default About;
